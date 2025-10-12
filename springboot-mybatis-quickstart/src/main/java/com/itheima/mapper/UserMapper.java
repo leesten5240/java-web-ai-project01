a097235a475c8c48da945ee2d8196e151f3f37a6
LeeSten_Desktop <leesten5240@foxmail.com>
2025-10-12 16:00:19 +0800

    feat(user): 增加用户增删改查功能
    - 添加根据id删除用户功能
    - 添加新增用户功能
    - 添加根据id更新用户信息功能
    - 添加根据用户名和密码查询用户信息功能- 完善用户相关测试用例

--- springboot-mybatis-quickstart/src/main/java/com/itheima/mapper/UserMapper.java
+++ springboot-mybatis-quickstart/src/main/java/com/itheima/mapper/UserMapper.java
@@ -1,13 +1,31 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
     @Select("select id, username, password, name, age from user")
-    public List<User> findAll();
+    List<User> findAll();
+
+    @Delete("delete from user where id = #{id}")
+    Integer deleteById(Integer id);
+
+    @Insert("insert into user(username,password,name,age) values(#{username},#{password},#{name},#{age}) ")
+    void insert(User user);
+
+    /**
+     * 根据id更新用户信息
+     */
+    @Update("update user set username = #{username},password = #{password},name = #{name},age = #{age} where id = #{id}")
+    void update(User user);
+
+    /**
+     * 根据用户名和密码查询用户信息
+     */
+    @Select("select * from user where username = #{username} and password = #{password}")
+    User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);//@Param注解用于给参数取别名，从而实现和SQL中参数的匹配
+
 }
--- springboot-mybatis-quickstart/src/test/java/com/itheima/SpringbootMybatisQuickstartApplicationTests.java
+++ springboot-mybatis-quickstart/src/test/java/com/itheima/SpringbootMybatisQuickstartApplicationTests.java
@@ -12,10 +12,41 @@ import java.util.List;
 class SpringbootMybatisQuickstartApplicationTests {
     @Autowired
     UserMapper userMapper;
+
+    /*
+    * 测试查询所有
+    * */
     @Test
     public void testFindAll(){
         List<User> userList = userMapper.findAll();
         userList.forEach(System.out::println);//System.out::println=(user)->System.out.println(user);
     }
 
+
+    /*
+    * 测试删除
+    * */
+    @Test
+    public void testDeleteById(){
+        Integer i = userMapper.deleteById(5);
+        System.out.println("执行完毕后影响的记录数："+  i);
+    }
+    /*
+     * 测试新增
+     * */
+    @Test
+    public void testInsert(){
+        User user = new User(null,"gaoyuanyuan","123456","高圆圆",18);
+        userMapper.insert(user);
+    }
+    @Test
+    public void testUpdate(){
+        User user = new User(6,"gaoyuanyuan","123456","高圆圆",25);
+        userMapper.update(user);
+    }
+    @Test
+    public void testFindByUsernameAndPassword(){
+        User user = userMapper.findByUsernameAndPassword("gaoyuanyuan", "123456");
+        System.out.println(user);
+    }
 }
